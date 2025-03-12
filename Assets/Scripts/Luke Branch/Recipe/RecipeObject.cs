c1866413743f81e56f9412a09fc52cbdac27ba8f
Ravdrew <andresegarcia@me.com>
2025-03-12 11:08:19 -0700

    try fix

--- Assets/Scripts/Luke Branch/Recipe/RecipeObject.cs
+++ Assets/Scripts/Luke Branch/Recipe/RecipeObject.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -22,17 +21,21 @@ public class RecipeObject : ScriptableObject
     {
         var otherRecipe = other as Dictionary<PreppedIngredientType, int>;
 
-        if(otherRecipe == null){
+        if (otherRecipe == null)
+        {
             return false;
         }
 
-        foreach (Ingredient ingredient in recipeIngredientList){
+        foreach (Ingredient ingredient in recipeIngredientList)
+        {
 
-            if(!otherRecipe.ContainsKey(ingredient.ingredientType)){
+            if (!otherRecipe.ContainsKey(ingredient.ingredientType))
+            {
                 return false;
             }
 
-            if(ingredient.count != otherRecipe[ingredient.ingredientType]){
+            if (ingredient.count != otherRecipe[ingredient.ingredientType])
+            {
                 return false;
             }
         }
@@ -43,13 +46,15 @@ public class RecipeObject : ScriptableObject
 }
 
 [Serializable]
-public struct Ingredient{
+public struct Ingredient
+{
     public PreppedIngredientType ingredientType;
 
     public int count;
 }
 
-public enum PreppedIngredientType {
+public enum PreppedIngredientType
+{
     LETTUCE,
     COOKED_PATTY,
 
